ef824eee193b9e2d43846a455d2ca385791539bc
Gabriel <gsotelosil14@gmail.com>
2025-08-08 17:23:21 -0700

    Added settings menu bar to change file convert save folder

---
+++ changesavelocation.cpp
@@ -0,0 +1,74 @@
+#include "changesavelocation.h"
+#include "ui_changesavelocation.h"
+#include <QFileDialog>
+#include <QMainWindow>
+#include <fstream>
+#include "json.hpp"
+
+using json = nlohmann::json;
+using namespace std;
+
+ChangeSaveLocation::ChangeSaveLocation(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::ChangeSaveLocation)
+{
+    ui->setupUi(this);
+    load_save_location();
+}
+
+ChangeSaveLocation::~ChangeSaveLocation()
+{
+    delete ui;
+}
+
+void ChangeSaveLocation::load_save_location()
+{
+    QString source_location = QString(__FILE__);
+    QFileInfo file_info(source_location);
+    QString cpp_directory = file_info.absolutePath();
+    QString json_path = cpp_directory + "/save_location.json";
+    ifstream save_json(json_path.toStdString());
+    json read_data;
+    save_json >> read_data;
+    QString save_folder = QString::fromStdString(read_data["location"].get<string>());
+    ui->save_location->setText(save_folder);
+    ui->save_location->setReadOnly(true);
+    ui->save_button->setEnabled(false);
+}
+
+void ChangeSaveLocation::on_change_folder_clicked()
+{
+    QString new_save_location = QFileDialog::getExistingDirectory(NULL, "Select Save Folder", QDir::homePath(), QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+    if (!new_save_location.isEmpty())
+    {
+        ui->save_location->setReadOnly(false);
+        ui->save_location->setText(new_save_location);
+        ui->save_location->setReadOnly(true);
+        ui->save_button->setEnabled(true);
+    }
+}
+
+
+void ChangeSaveLocation::on_save_button_clicked()
+{
+    QString source_location = QString(__FILE__);
+    QFileInfo file_info(source_location);
+    QString cpp_directory = file_info.absolutePath();
+    QString json_path = cpp_directory + "/save_location.json";
+    json save_data;
+    save_data["location"] = (ui->save_location->toPlainText()).toStdString();
+    ofstream file(json_path.toStdString());
+    if (file.is_open())
+    {
+        file << save_data.dump(4);
+        file.close();
+        ui->save_button->setEnabled(false);
+    }
+}
+
+
+void ChangeSaveLocation::on_cancel_button_clicked()
+{
+    this->close();
+}
+
